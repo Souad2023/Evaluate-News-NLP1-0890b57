fa2bcc2456fd522da58b5aa797648e42389f467f
souad <souadhuss3@gmail.com>
2024-12-09 05:17:12 -0800

    Update test/formHandler.test.js and delete New Text Document.txt

--- test/formHandler.test.js
+++ test/formHandler.test.js
@@ -1,9 +1,9 @@
 import { isValidURL } from "../src/client/js/formHandler";
 
 describe("Testing form submission", () => {
-  // محاكاة DOM وإنشاء العنصر قبل كل اختبار
+ 
   beforeEach(() => {
-    // إنشاء عنصر form وهمي في البيئة
+    
     document.body.innerHTML = `
       <form id="urlForm">
         <input id="name" type="text" name="url" placeholder="Enter URL" required>
@@ -16,19 +16,18 @@ describe("Testing form submission", () => {
   test("should call handleSubmit on form submit", () => {
     const form = document.getElementById("urlForm");
 
-    // التأكد من أن العنصر موجود
+    
     expect(form).not.toBeNull();
 
-    // هنا نقوم بإنشاء دالة مقلدة (mock function) للتأكد من أنه تم استدعاؤها
+    
     const handleSubmit = jest.fn();
 
-    // إضافة مستمع الحدث "submit" إلى النموذج
     form.addEventListener("submit", handleSubmit);
 
-    // محاكاة تقديم النموذج
+    
     form.dispatchEvent(new Event("submit"));
 
-    // التأكد من أن الدالة قد تم استدعاؤها مرة واحدة
+    
     expect(handleSubmit).toHaveBeenCalledTimes(1);
   });
 
